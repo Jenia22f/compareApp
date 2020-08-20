020bfc5d45677e8cea191e8dd8ceea5333eb4ee7
Jenia22f <Nofightnoglory22>
2020-08-20 17:05:56 +0300

    local mongoDB

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    mongoURI: 'mongodb://adminCloacka:cloacka777Cloacka@localhost:27017/compareApp'
+    mongoURI: 'mongodb://adminCloacka:cloacka777Cloacka@139.59.159.43:27017/compareApp?authSource=admin&readPreference=primary&appname=MongoDB%20Compass&ssl=false'
 }
