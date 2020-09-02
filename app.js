008fd58ae3c461ab27b79eecd74d85e5de46d05f
Jenia22f <Nofightnoglory22>
2020-09-02 16:55:43 +0300

    local mongoDB

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(morgan('combined', {
             originalUrl: req.originalUrl,
             status: res.statusCode,
             userAgent: req.headers['user-agent'],
-            application: req.application
+            application: req.body.application
         })
         log.save()
     }
