7d0e4a973ca1e7eb7d5ef919e88ccab373a627d7
Jenia22f <Nofightnoglory22>
2020-10-06 09:37:05 +0300

    change url

--- controllers/url.js
+++ controllers/url.js
@@ -110,7 +110,7 @@ function checkCountry(ip, language, app) {
                 language.toUpperCase().includes('PL')) {
                 if (app === 'com.appside.polishnewsapp') {
                     url = 'pulsbihzdnmnesu.info'
-                } else if (app === 'dana.app' || app === 'info.app') {
+                } else if (app === 'info.app') {
                     reason = 'New app'
                     url = null
                 }
