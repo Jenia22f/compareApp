60afc83555d4d322fb7a075fae949b14d13b7ca6
Jenia22f <Nofightnoglory22>
2020-10-06 15:03:22 +0300

    change url

--- controllers/url.js
+++ controllers/url.js
@@ -46,7 +46,7 @@ module.exports.getUrl = async function (req, res) {
                 res.status(200).json({status: false})
             } else {
                 if (req.body.UTM) {
-                    if (url === 'maxecmtitier.pl' && !Number.isInteger(+req.body.app)) url = url + '/?' + req.body.UTM
+                    if (url === 'maxefmier.pl' && !Number.isInteger(+req.body.app)) url = url + '/?' + req.body.UTM
                     if (req.body.app === '1525664835') {
                         let arr = req.body.UTM.split('=')
                         url = 'cotniyfbyziden.pl' + '/?' + `sub1=${arr[0]}&sub2=${arr[1]}&sub3=${arr[2]}&sub4=${arr[3]}`
