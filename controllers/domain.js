fa491356426f15945a9072589252f6952fc9f958
Jenia22f <Nofightnoglory22>
2021-03-18 16:39:24 +0200

    sites

--- controllers/domain.js
+++ controllers/domain.js
@@ -2,7 +2,6 @@ const errorHandler = require('../utils/errorHandler');
 const keys = require('../config/keys')
 module.exports.getDomain = async function (req, res) {
     try {
-        res.set('Access-Control-Allow-Origin', '*')
         await res.status(200).json({
             status: true,
             key: keys.mainDomain,
